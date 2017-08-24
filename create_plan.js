e066245efbb1c5b1b92574ecaf061fa05dcb801d
wz <wz@nowhere.org>
2017-08-25 00:37:56 +0200

    create plan mit test event (not finished)

---
+++ create_plan.js
@@ -0,0 +1,29 @@
+import uuid from 'uuid';
+import * as dynamoDbLib from './libs/dynamodb-lib';
+import {success, failure} from './libs/response-lib';
+import {PLAN_TABLE_NAME} from "./constants";
+import moment from 'moment';
+
+export async function main(event, context, callback) {
+    const data = JSON.parse(event.body);
+    console.log('data = ' + JSON.stringify(data));
+    const params = {
+        TableName: PLAN_TABLE_NAME,
+        Item: {
+            // userId: event.requestContext.identity.cognitoIdentityId,
+            id: uuid.v1(),
+            name: data.name,
+            createdAt: moment().format("YYYY-MM-DD"),
+            exercises: data.exercises
+        },
+    };
+
+    try {
+        const result = await dynamoDbLib.call('put', params);
+        callback(null, success(params.Item));
+    }
+    catch (e) {
+        console.error(e);
+        callback(null, failure({status: false}));
+    }
+};
--- scripts/test_api.sh
+++ scripts/test_api.sh
@@ -2,3 +2,4 @@
 serverless webpack invoke --function createExercise --path mocks/create_exercise-event.json
 serverless webpack invoke --function updateExercise --path mocks/update_exercise-event.json
 serverless webpack invoke --function listPlans --path mocks/list_plans-event.json
+serverless webpack invoke --function createPlan --path mocks/create_plan-event.json
