352144baa25667eb37a70f1dc28712eed3ee6959
wz <wz@nowhere.org>
2017-08-24 23:32:40 +0200

    list plans mit test event

--- constants.js
+++ constants.js
@@ -1,1 +1,2 @@
 export const EXERCISE_TABLE_NAME = 'fit_exercises';
+export const PLAN_TABLE_NAME = 'fit_training_plan';
--- scripts/test_api.sh
+++ scripts/test_api.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 serverless webpack invoke --function createExercise --path mocks/create_exercise-event.json
 serverless webpack invoke --function updateExercise --path mocks/update_exercise-event.json
+serverless webpack invoke --function listPlans --path mocks/list_plans-event.json
