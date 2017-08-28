84856178c098d417ba249fa7477d349d7ad58548
Wolfgang Zolleis <wzolleis@web.de>
2017-08-28 22:12:29 +0200

    umbenennen der handler um besser gruppieren zu koennen

--- scripts/test_api.sh
+++ scripts/test_api.sh
@@ -2,6 +2,6 @@
 serverless webpack invoke --function createExercise --path mocks/create_exercise-event.json
 serverless webpack invoke --function updateExercise --path mocks/update_exercise-event.json
 serverless webpack invoke --function listPlans --path mocks/list_plans-event.json
-serverless webpack invoke --function getExercise --path mocks/get_exercise-event.json
 serverless webpack invoke --function createPlan --path mocks/create_plan-event.json
 serverless webpack invoke --function getPlan --path mocks/get_plan-event.json
+serverless webpack invoke --function listExercises --path mocks/list_plans-event.json
