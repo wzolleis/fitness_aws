cb04a4e91dd274aa57f4926660cd7ffb3ea84036
wz <wz@nowhere.org>
2017-09-02 19:39:19 +0200

    create plan event

--- plan_create.js
+++ plan_create.js
@@ -17,6 +17,7 @@ export async function main(event, context, callback) {
             exercises: data.exercises
         },
     };
+    console.log('params = ' + JSON.stringify(params));
 
     try {
         const result = await dynamoDbLib.call('put', params);
