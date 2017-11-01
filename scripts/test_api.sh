108ae01a4eb4adbe6c373d9c8f0c66442b04f9f1
Wolfgang Zolleis <wzolleis@web.de>
2017-11-01 23:31:32 +0100

    training mit finishedEvent

--- scripts/test_api.sh
+++ scripts/test_api.sh
@@ -5,7 +5,7 @@ serverless webpack invoke --function listPlans --path mocks/list_plans-event.jso
 serverless webpack invoke --function createPlan --path mocks/create_plan-event.json
 serverless webpack invoke --function getPlan --path mocks/get_plan-event.json
 serverless webpack invoke --function listExercises --path mocks/list_plans-event.json
-serverless webpack invoke --function updatePlan --path mocks/lupdate_plan-event.json
+serverless webpack invoke --function updatePlan --path mocks/update_plan-event.json
 serverless webpack invoke --function createTraining --path mocks/create_training-event.json
 serverless webpack invoke --function listTrainings --path mocks/list_trainings-event.json
-serverless webpack invoke --function updateTraining --path mocks/lupdate_training-event.json
+serverless webpack invoke --function updateTraining --path mocks/update_training-event.json
